1a1c433366a24b11b55c4d4ad0afa87e33b2a672
Berkan Serbes <berkan_serbes@hotmail.com>
2025-06-05 09:42:45 +0300

    Add RabbitMQ.Client and implement consumer/producer
    Updated DirectExchange.Consumer and DirectExchange.Producer projects to include RabbitMQ.Client version 7.1.2 for message queuing.
    
    Replaced console output in Program.cs of both projects with code to set up a RabbitMQ consumer and producer, respectively. The consumer connects to a RabbitMQ server, declares a direct exchange, creates a queue, and binds it to the exchange. The producer connects to the server, declares the exchange, and allows user input for message publishing.

--- c-sharp/RabbitMQExchangeTypes/DirectExchange.Consumer/Program.cs
+++ c-sharp/RabbitMQExchangeTypes/DirectExchange.Consumer/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Direct Exchange - Consumer");
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "direct-exchange-example",
+	type: ExchangeType.Direct,
+	durable: false,
+	autoDelete: false,
+	noWait: false
+);
+
+var newQueue = await channel.QueueDeclareAsync(
+	queue: "direct-exchange-queue",
+	durable: false,
+	exclusive: false,
+	autoDelete: false,
+	noWait: false
+);
+
+string queueName = newQueue.QueueName;
+
+await channel.QueueBindAsync(
+	queue: queueName,
+	exchange: "direct-exchange-example",
+	routingKey: "direct-routing-key"
+);
+
+AsyncEventingBasicConsumer consumer = new(channel);
+
+consumer.ReceivedAsync += (sender, e) =>
+{
+	string message = Encoding.UTF8.GetString(e.Body.Span);
+	Console.WriteLine($"Received message: {message}");
+	return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+	queue: queueName,
+	autoAck: false,
+	consumer: consumer
+);
+
+Console.ReadLine();
--- c-sharp/RabbitMQExchangeTypes/DirectExchange.Producer/Program.cs
+++ c-sharp/RabbitMQExchangeTypes/DirectExchange.Producer/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Net;
+using System.Text;
+
+Console.WriteLine("Direct Exchange - Publisher");
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "direct-exchange-example",
+	type: ExchangeType.Direct,
+	durable: false,
+	autoDelete: false,
+	noWait: false
+);
+
+while (true)
+{
+	Console.Write("Enter a message: ");
+	string message = Console.ReadLine()!;
+	byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+	await channel.BasicPublishAsync(
+		exchange: "direct-exchange-example",
+		routingKey: "direct-routing-key",
+		body: byteMessage
+	);
+}
