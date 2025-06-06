8719086b3e4a278b653fdad633c4c9b1fa7b37a9
Berkan Serbes <berkan_serbes@hotmail.com>
2025-06-06 15:21:32 +0300

    Add C# Header Exchange example

--- c-sharp/RabbitMQExchangeTypes/HeaderExchange.Consumer/Program.cs
+++ c-sharp/RabbitMQExchangeTypes/HeaderExchange.Consumer/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP Address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "header-exchange-example",
+	type: ExchangeType.Headers
+);
+
+Console.Write("Enter header value: ");
+var headerValue = Console.ReadLine();
+
+var queue = await channel.QueueDeclareAsync();
+string queueName = queue.QueueName;
+
+await channel.QueueBindAsync(
+	queue: queueName,
+	exchange: "header-exchange-example",
+	routingKey: string.Empty,
+	arguments: new Dictionary<string, object?>
+	{
+		{ "x-match", "all" }, // or "any" for any match
+		{ "header-key", headerValue }
+	}
+);
+
+AsyncEventingBasicConsumer consumer = new(channel);
+
+consumer.ReceivedAsync += (sender, e) =>
+{
+	string message = Encoding.UTF8.GetString(e.Body.Span);
+	Console.WriteLine($"Received message: {message}");
+	return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+	queue: queueName,
+	autoAck: true,
+	consumer: consumer
+);
+
+Console.Read();
--- c-sharp/RabbitMQExchangeTypes/HeaderExchange.Producer/Program.cs
+++ c-sharp/RabbitMQExchangeTypes/HeaderExchange.Producer/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Net;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP Address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "header-exchange-example",
+	type: ExchangeType.Headers
+);
+
+for (int i = 0; i < 20; i++)
+{
+	await Task.Delay(200);
+	byte[] message = Encoding.UTF8.GetBytes($"Message {i + 1}");
+
+	Console.Write("Enter header value: ");
+	var headerValue = Console.ReadLine();
+
+	var basicProperties = new BasicProperties
+	{
+		Headers = new Dictionary<string, object?>
+		{
+			["header-key"] = headerValue
+		}
+	};
+
+	await channel.BasicPublishAsync(
+		exchange: "header-exchange-example",
+		routingKey: string.Empty,
+		body: message,
+		basicProperties: basicProperties,
+		mandatory: false
+	);
+}
+
+Console.Read();
