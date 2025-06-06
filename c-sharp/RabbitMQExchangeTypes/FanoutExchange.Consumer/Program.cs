4b575197508e3569eb2270891f60ef8f8acfc8c4
Berkan Serbes <berkan_serbes@hotmail.com>
2025-06-06 11:23:40 +0300

    Update RabbitMQ integration for consumer and producer
    - Upgrade project to .NET 9.0 and add RabbitMQ.Client v7.1.2.
    - Implement message consumption in the consumer's Program.cs, allowing user to specify a queue name.
    - Implement message publishing in the producer's Program.cs, sending ten messages to a fanout exchange with delays.

--- c-sharp/RabbitMQExchangeTypes/FanoutExchange.Consumer/Program.cs
+++ c-sharp/RabbitMQExchangeTypes/FanoutExchange.Consumer/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP Address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "fanout-exchange-example",
+	type: ExchangeType.Fanout
+);
+
+Console.Write("Kuyruk adını giriniz: ");
+string queueName = Console.ReadLine()!;
+
+await channel.QueueDeclareAsync(
+	queue: queueName,
+	exclusive: false
+);
+
+await channel.QueueBindAsync(
+	queue: queueName,
+	exchange: "fanout-exchange-example",
+	routingKey: string.Empty
+);
+
+AsyncEventingBasicConsumer consumer = new(channel);
+
+consumer.ReceivedAsync += (sender, e) =>
+{
+	string message = Encoding.UTF8.GetString(e.Body.Span);
+	Console.WriteLine($"Received message: {message}");
+	return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+	queue: queueName,
+	autoAck: false,
+	consumer: consumer
+);
+
+Console.ReadLine();
--- c-sharp/RabbitMQExchangeTypes/FanoutExchange.Producer/Program.cs
+++ c-sharp/RabbitMQExchangeTypes/FanoutExchange.Producer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP Address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "fanout-exchange-example",
+	type: ExchangeType.Fanout
+);
+
+for(int i = 0; i < 10; i++)
+{
+	await Task.Delay(200);
+	byte[] byteMessage = Encoding.UTF8.GetBytes($"Message {i + 1}");
+
+	await channel.BasicPublishAsync(
+		exchange: "fanout-exchange-example",
+		routingKey: string.Empty,
+		body: byteMessage
+	);
+}
+
+Console.ReadLine();
