9e6ad69c94a48aa936274beb6dd8a36db1fb13ce
Berkan Serbes <berkan_serbes@hotmail.com>
2025-06-06 15:20:03 +0300

    Add C# Topic Exchange example

--- c-sharp/RabbitMQExchangeTypes/TopicExchange.Consumer/Program.cs
+++ c-sharp/RabbitMQExchangeTypes/TopicExchange.Consumer/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP Address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "topic-exchange-example",
+	type: ExchangeType.Topic
+);
+
+Console.Write("Enter topic format: ");
+string topic = Console.ReadLine()!;
+
+var queue = await channel.QueueDeclareAsync();
+string queueName = queue.QueueName;
+
+await channel.QueueBindAsync(
+	queue: queueName,
+	exchange: "topic-exchange-example",
+	routingKey: topic
+);
+
+AsyncEventingBasicConsumer consumer = new(channel);
+
+consumer.ReceivedAsync += (sender, e) =>
+{
+	string message = Encoding.UTF8.GetString(e.Body.Span);
+	Console.WriteLine($"Received message: {message}");
+	return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+	queue: queueName,
+	consumer: consumer,
+	autoAck: true
+);
+
+Console.Read();
--- c-sharp/RabbitMQExchangeTypes/TopicExchange.Producer/Program.cs
+++ c-sharp/RabbitMQExchangeTypes/TopicExchange.Producer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP Address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "topic-exchange-example",
+	type: ExchangeType.Topic
+);
+
+for (int i = 0; i < 20; i++)
+{
+	await Task.Delay(200);
+	byte[] message = Encoding.UTF8.GetBytes($"Message {i + 1}");
+
+	Console.Write("Enter routing key: ");
+	string routingKey = Console.ReadLine()!;
+
+	await channel.BasicPublishAsync(
+		exchange: "topic-exchange-example",
+		routingKey: routingKey,
+		body: message
+	);
+}
+
+Console.Read();
