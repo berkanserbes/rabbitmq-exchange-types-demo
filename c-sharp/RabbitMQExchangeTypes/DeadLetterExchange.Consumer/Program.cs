bbcebe76ed364c25f5d446f6f88a66a28628e24f
Berkan Serbes <berkan_serbes@hotmail.com>
2025-06-09 10:38:30 +0300

    Add Dead-letter Exchange consumer and producer implementations in C#

---
+++ c-sharp/RabbitMQExchangeTypes/DeadLetterExchange.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP Address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "dlx-exchange",
+	type: ExchangeType.Direct
+);
+
+await channel.QueueDeclareAsync(
+	queue: "dlx-queue",
+	durable: true
+);
+
+await channel.QueueBindAsync(
+	queue: "dlx-queue",
+	exchange: "dlx-exchange",
+	routingKey: "dlx-routing"
+);
+
+AsyncEventingBasicConsumer consumer = new(channel);
+
+consumer.ReceivedAsync += (sender, e) =>
+{
+	string message = Encoding.UTF8.GetString(e.Body.Span);
+	Console.WriteLine($"DLX received dead-lettered message: {message}");
+	return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+	queue: "dlx-queue",
+	autoAck: true,
+	consumer: consumer
+);
+
+Console.Read();
---
+++ c-sharp/RabbitMQExchangeTypes/DeadLetterExchange.Producer/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new(<Your CloudAMQP Address>);
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+	exchange: "main-exchange",
+	type: ExchangeType.Direct
+);
+
+await channel.ExchangeDeclareAsync(
+	exchange: "dlx-exchange",
+	type: ExchangeType.Direct
+);
+
+await channel.QueueDeclareAsync(
+	queue: "main-queue",
+	durable: true,
+	arguments: new Dictionary<string, object?>
+	{
+		{ "x-dead-letter-exchange", "dlx-exchange" },
+		{ "x-dead-letter-routing-key", "dlx-routing" },
+		{ "x-message-ttl", 5000 }
+    }
+);
+
+await channel.QueueBindAsync(
+	queue: "main-queue",
+	exchange: "main-exchange",
+	routingKey: "main-routing-key"
+);
+
+for (int i = 0; i < 10; i++)
+{
+	
+	var message = $"Message {i + 1}";
+	var body = Encoding.UTF8.GetBytes(message);
+
+	await channel.BasicPublishAsync(
+		exchange: "main-exchange",
+		routingKey: "main-routing-key",
+		body: body
+	);
+
+	Console.WriteLine($"Sent: {message}");
+	await Task.Delay(1000);
+}
+
+Console.Read();
